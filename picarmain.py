46d4ddd51ad1ce13a4c89f4d0158fc6f7ef5fd63
drshivago <moessner2015@gmail.com>
2017-01-18 09:45:06 -0500

    tweeked canny func

--- picarmain.py
+++ picarmain.py
@@ -9,16 +9,15 @@ camera = PiCamera(resolution = (640,480),framerate = 32)
 camera.vflip = True
 rawCapture = PiRGBArray(camera, size=(640,480))
 time.sleep(0.5)
-threshold1 = 20
-Max_threshold = 100
+thresholdlow = 20
+thresholdhigh = 70
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
         image = frame.array    #image is now a nparray
-        detected_edges = cv2.blur(image, (3,3))
-        detected_edges2 = cv2.Canny(detected_edges, threshold1, threshold1*3, apertureSize=3)
+        detected_edges = cv2.Canny(image, thresholdlow, thresholdhigh, apertureSize=3)
 
-        cv2.imshow("Frame", detected_edges2)
+        cv2.imshow("Frame", detected_edges)
 
 
         key = cv2.waitKey(1) & 0xFF
