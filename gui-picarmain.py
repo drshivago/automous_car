7bcdf2fa93df32b9493de763a2849ccf98c6e741
drshivago <moessner2015@gmail.com>
2017-01-24 23:55:55 -0500

    fixed control

--- gui-picarmain.py
+++ gui-picarmain.py
@@ -7,6 +7,7 @@ import cv2
 
 camera = PiCamera(resolution = (640,480),framerate = 32)
 camera.vflip = True
+camera.hflip = True
 rawCapture = PiRGBArray(camera, size=(640,480))
 time.sleep(0.5)
 threshold1 = 20
@@ -19,7 +20,6 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         detected_edges2 = cv2.Canny(detected_edges, threshold1, threshold1*3, apertureSize=3)
 
         cv2.imshow("Frame", detected_edges2)
-
 
         key = cv2.waitKey(1) & 0xFF
         rawCapture.truncate(0)
---
+++ tools/control-test.py
@@ -0,0 +1,73 @@
+import RPi.GPIO as GPIO
+import picamera
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+#drive
+#forward
+GPIO.setup(11, GPIO.OUT)
+#backward
+GPIO.setup(12, GPIO.OUT)
+#steering
+#left
+GPIO.setup(15, GPIO.OUT)
+#right
+GPIO.setup(16, GPIO.OUT)
+
+camera = picamera.PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 30
+camera.vflip = True
+
+time.sleep(2)
+#camera.start_recording('drivingvideo.h264', format='h264')
+camera.start_preview()
+key = ''
+forard = False
+bakk = False
+key = 'n'
+direct = ''
+side = ''
+while key != 'q':
+    key = raw_input()
+    if key == 'w':
+        direct = 'forward'
+    if forard:
+        GPIO.output([15, 16], GPIO.LOW)
+        forard = False
+        side = ''
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(11, GPIO.HIGH)
+    forard = True
+    bakk = False
+    if key == 'a':
+        side = 'left'
+        GPIO.output(16, GPIO.LOW)
+        GPIO.output(15, GPIO.HIGH)
+    if key == 'd':
+        side = 'right'
+        GPIO.output(15, GPIO.LOW)
+        GPIO.output(16, GPIO.HIGH)
+    if key == 's':
+        direct = 'reverse'
+    if bakk:
+        GPIO.output([15, 16], GPIO.LOW)
+        bakk = False
+        side = ''
+    GPIO.output(11, GPIO.LOW)                
+    GPIO.output(12, GPIO.HIGH)
+    bakk = True
+    forard = False
+    if key == 'f':
+        direct = 'full stop'
+        side = ''
+        GPIO.output([11, 12, 15, 16], GPIO.LOW)
+        forard = False
+        bakk = False
+    camera.annotate_text = direct + ' ' + side
+camera.stop_preview()
+#camera.wait_recording(3)
+#camera.stop_recording()
+
+GPIO.cleanup()
