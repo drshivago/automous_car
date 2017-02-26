61da8ad3114e6ee6b6fc0438ff14e34766fd71ac
rex <rex@localhost.localdomain>
2017-02-26 15:22:34 -0500

    fixed bug

--- cardrive_server.py
+++ cardrive_server.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
+
 import io
 import socket
 import struct
-from PIL import image
+
 
 server_socket = socket.socket()
-server_socket.bind((’0.0.0.0’, 8000))
+server_socket.bind(('0.0.0.0', 8000))
 server_socket.listen(0)
 
-connection = server_socket.accept()[0].makefile(‘rb’)
+connection = server_socket.accept()[0].makefile('rb')
 try:
 	while True:
-		image_len - struct.unpack(‘<L’, connection.read(struct.calcsize(‘<L’)))[0]
+		image_len - struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
 		if not image_len:
 			break
 		image_stream = io.BytesIO()
@@ -19,9 +19,9 @@ try:
 
 		image_stream.seek(0)
 		image = Image.open(image_stream)
-		print(‘Image is %dx%d’ % image.size)
+		print('Image is %dx%d' % image.size)
 		image.verify()
-		print(‘Image is verified’)
+		print('Image is verified')
 finally:
 	connection.close()
 	server_socket.close()
