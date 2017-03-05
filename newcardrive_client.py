7385482ecd5a67f14440336ae7db370fd04c888a
drshivago <moessner2015@gmail.com>
2017-03-05 01:11:52 -0500

    fixed syntax

--- newcardrive_client.py
+++ newcardrive_client.py
@@ -7,9 +7,9 @@ import picamera
 rez = (640,480)
 
 client_socket = socket.socket()	#create a new socket
-client_socket.connect((192.168.2.21, 8000))	#whatever tank ip is
+client_socket.connect(('192.168.2.21', 8000))	#whatever tank ip is
 
-connection = client_socket.makefile(‘w’)
+connection = client_socket.makefile('w')
 try:
 	camera = picamera.PiCamera()
 	camera.resolution = rez
@@ -17,7 +17,7 @@ try:
 	
 	camera.start_recording(connection, format= h264)
 
-finnaly:
+finally:
 	camera.stop_recording()
 	connection.close()
 	client_socket.close()
